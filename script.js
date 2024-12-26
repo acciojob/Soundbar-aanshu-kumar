fd7d8cdc0465fba6c269664df23b37e87923573d
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-26 18:14:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let Applause = document.getElementById("Applause")
+let boo = document.getElementById("boo")
+let gasp = document.getElementById("gasp")
+let tada = document.getElementById("tada")
+let victory = document.getElementById("victory")
+let wrong = document.getElementById("wrong")
+
+let stop = document.getElementsByClassName("stop");
+stop.addEventListner("click",()=>{
+    Applause.pause();
+    boo.pause();
+    gasp.pause();
+    tada.pause();
+    victory.pause();
+    wrong.pause();
+})
+
+let btn = document.getElementsByClassName("btn");
+
+for(button in btn){
+    button.addEventListner("click",()=>{
+        Applause.play();
+    })
+}
